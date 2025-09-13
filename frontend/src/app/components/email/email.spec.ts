4012876772b604c990501d8635ffd7c71f917208
saniyasarfaraz <saniyasarfaraz61@gmail.com>
2025-09-13 19:05:57 +0530

    Add Continue with Email Functionality

--- frontend/src/app/components/email/email.spec.ts
+++ frontend/src/app/components/email/email.spec.ts
@@ -1,26 +1,3 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-// import { Email } from './email';
-
-// describe('Email', () => {
-//   let component: Email;
-//   let fixture: ComponentFixture<Email>;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [Email]
-//     })
-//     .compileComponents();
-
-//     fixture = TestBed.createComponent(Email);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
--- frontend/src/app/components/email/email.ts
+++ frontend/src/app/components/email/email.ts
@@ -1,21 +1,3 @@
-// import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-email',
-//   imports: [],
-//   templateUrl: './email.html',
-//     changeDetection: ChangeDetectionStrategy.OnPush,
-
-//   styleUrl: './email.css'
-// })
-// export class Email {
-//  handleContinue() {
-//     // Logic for the continue button can be added here
-//     console.log('Continue button clicked!');
-//   }
-// }
-
-
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
